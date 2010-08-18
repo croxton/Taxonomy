95e078ab927ac5c8f7982bf29a28055675a2f602
Iain Urquhart <shout@iain.co.nz>
2010-08-19 10:48:39 +1200

    Add active css class to relative links that are placed via url override

--- libraries/mpttree.php
+++ libraries/mpttree.php
@@ -2544,19 +2544,25 @@ ORDER BY {$this->left_col} DESC) as parent";
 	    		{
 	    			$template_name = '';
 	    		}
-	    		
+
 	    		$node_url = 	$this->EE->functions->fetch_site_index().$template_group.$template_name.$url_title;
-	    		// if we're not using an index, get rid of double slashes
-				$node_url = $this->EE->functions->remove_double_slashes($node_url);
 	    		
 	    		$viewed_url = 	$this->EE->functions->fetch_site_index().'/'.$this->EE->uri->uri_string();
-	    		$viewed_url = 	$this->EE->functions->remove_double_slashes($viewed_url);
-	    		
+
 	    		// override template and entry slug with custom url if set
 	    		if($data['custom_url'] != '')
 	    		{
 	    			$node_url = $data['custom_url'];
+	    			
+	    			// does the custom url start with http://, 
+	    			// if not we add our site_index as it'll be a relative link
+	    			// and the nav tag will apply the $active css class to the node
+	    			$node_url = (substr(ltrim($node_url), 0, 7) != 'http://' ? $this->EE->functions->fetch_site_index() : '') . $node_url;
 	    		}
+	    		
+	    		// get rid of double slashes
+				$node_url 	= $this->EE->functions->remove_double_slashes($node_url);
+				$viewed_url = $this->EE->functions->remove_double_slashes($viewed_url);
 	    						
 	    		if($node_url == $viewed_url)
 	    		{
