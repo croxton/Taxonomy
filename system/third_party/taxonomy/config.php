0c547f3f080e67f018bed62a980368bae4f81573
Iain Urquhart <shout@iain.co.nz>
2010-11-17 09:28:08 +1300

    Version numbers

--- system/third_party/taxonomy/config.php
+++ system/third_party/taxonomy/config.php
@@ -1,4 +1,4 @@
 <?php
 $config['name']='Taxonomy';
-$config['version']='1.06';
+$config['version']='1.062';
 $config['nsm_addon_updater']['versions_xml']='http://iain.co.nz/taxonomy/versions.xml';
--- system/third_party/taxonomy/upd.taxonomy.php
+++ system/third_party/taxonomy/upd.taxonomy.php
@@ -13,7 +13,7 @@
  */
 class Taxonomy_upd {
 
-	var $version        = '1.061';
+	var $version        = '1.062';
 	var $module_name 	= "Taxonomy";
 	
 	function Taxonomy_upd( $switch = TRUE ) 
